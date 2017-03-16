500c02a975703bd2edf7e9ca7736d60a4bd8a02b
Petter Eik <PetterEik@petters-mbp-2.lan>
2017-03-16 12:17:01 +0100

    Initial Commit

---
+++ StaySocial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StaySocial
+//
+//  Created by Petter Eik on 16.03.2017.
+//  Copyright © 2017 Petter Eik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
